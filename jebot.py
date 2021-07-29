d8acc5418dd2cc3a5c47e31553d083c47cc4d54a
Tamilventhan Mec <87693241+agamudaiyadevan@users.noreply.github.com>
2021-07-29 13:06:26 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -110,7 +110,7 @@ async def telegraphphoto(client, message):
 
 @Jebot.on_message(filters.video)
 async def telegraphvid(client, message):
-    msg = await message.reply_text("Uploading To Telegraph...)
+    msg = await message.reply_text("Uploading To Telegraph...")
     download_location = await client.download_media(
         message=message, file_name='root/jetg')
     try:
